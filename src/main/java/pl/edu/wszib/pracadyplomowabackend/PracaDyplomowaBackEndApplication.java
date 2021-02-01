f25494245fafe359c7714edb619ec0f9194aaaad
pkornijasz <pk@nkk.pl>
2021-02-01 08:36:53 +0100

    Initial commit

---
+++ src/main/java/pl/edu/wszib/pracadyplomowabackend/PracaDyplomowaBackEndApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.pracadyplomowabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaDyplomowaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaDyplomowaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/pracadyplomowabackend/PracaDyplomowaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.pracadyplomowabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaDyplomowaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
